03358df95b4e86e1fa6b8c58ca7c95dcfdbbcfd0
Christopher Reid <chrisreid657@gmail.com>
2023-04-24 15:09:28 -0500

    JS demo

---
+++ Lecture-demos/First code.js
@@ -0,0 +1,56 @@
+
+
+var firstname;
+
+firstname = "Chris";
+
+middlename = "Jordan";
+
+lastname = "Reid";
+
+
+
+var greeting; //This line will assign the result of an expression to the greeting variable//
+
+greeting = ("Your name is " + firstname);
+
+console.log(greeting);
+
+
+var customerid;
+customerid="00112"
+
+var fml
+fml = (firstname + middlename + lastname);
+
+var DOB
+DOB="01/05/1999";
+
+var dl
+dl="20394820";
+
+var Auto
+Auto="1019192838"
+
+var Gender
+Gender="male";
+
+
+
+
+
+
+
+
+
+
+
+console.log(customerid)
+console.log(fml)
+console.log(DOB)
+console.log(Gender)
+console.log(dl)
+console.log(Auto)
+
+
+
